4af086f42cff135e991de283af69315204325c26
bhuns57 <bhun1957@gmail.com>
2025-07-20 17:25:28 -0500

    added and tested input function revise organization

--- shared_data.py
+++
@@ -1,9 +0,0 @@
-#🧠 Option 3: Use a shared Python module
-# Create a Python file (e.g. shared_data.py)
-# Save df_tbl creation code in it
-
-#Import it from other notebooks:
-#"from shared_data import df_tbl"
-python
-# load line in user modules = from shared_data import df_tbl
-⚠️ Requires re-execution, not storage-based reuse
